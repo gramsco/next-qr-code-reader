d7ad465982a94d4bf18bbfb4911f183831dc5dc8
gramsco <antoningauth@gmail.com>
2020-12-19 18:06:27 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,50 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const videoRef = useRef();
+
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    if (!result) return;
+    let url;
+    try {
+      url = new URL(result);
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+    window.location.href = url;
+  }, [result]);
+
+  function launchQR() {
+    setResult("loading module...");
+    import("qr-scanner").then((module) => {
+      let id;
+      setResult("module loaded");
+      let QrScanner = module.default;
+      const qrScanner = new QrScanner(videoRef.current, (result) => {
+        setResult(result);
+        qrScanner.stop();
+        alert("???");
+        clearInterval(id);
+      });
+      setResult("Looking for results");
+      qrScanner.start();
+      id = setInterval(() => {
+        setResult("no result found :(");
+        qrScanner.stop();
+      }, 5000);
+    });
+  }
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h1>QR Code Reader</h1>
+      <h2>{result}</h2>
+      <button onClick={launchQR}>Launch</button>
+      <video width="500" height="500" ref={videoRef} />
     </div>
-  )
+  );
 }
